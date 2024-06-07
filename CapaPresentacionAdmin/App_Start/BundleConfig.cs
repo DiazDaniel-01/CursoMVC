af940f01030ca9175bd334d916ce4fc5742dceb5
DiazDaniel-01 <diazdaniel.alej@gmail.com>
2024-06-07 17:59:38 -0300

    Se realizo el video 12 del curso, se agrego un paquete nugget y nuevas extensiones js

--- CapaPresentacionAdmin/App_Start/BundleConfig.cs
+++ CapaPresentacionAdmin/App_Start/BundleConfig.cs
@@ -15,6 +15,8 @@ namespace CapaPresentacionAdmin
                         "~/Scripts/fontawesome/all.min.js",
                         "~/Scripts/DataTables/jquery.dataTables.js",
                         "~/Scripts/DataTables/dataTables.responsive.js",
+                        "~/Scripts/loadingoverlay/loadingoverlay.min.js",
+                        "~/Scripts/sweetalert.min.js",
                         "~/Scripts/scripts.js"));
 
             // bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -31,7 +33,8 @@ namespace CapaPresentacionAdmin
             bundles.Add(new StyleBundle("~/Content/css").Include( 
                 "~/Content/site.css",
                 "~/Content/DataTables/css/jquery.dataTables.css",
-                "~/Content/DataTables/css/responsive.dataTables.css"
+                "~/Content/DataTables/css/responsive.dataTables.css",
+                "~/Content/sweetalert.css"
                 ));
         }
     }
