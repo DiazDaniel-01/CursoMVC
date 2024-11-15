11a041ea801b4586ce61a6f5979c8494b4af12ea
HernanD98 <hernan_duranti@hotmail.com>
2024-11-15 18:56:09 -0300

    actualizacion

--- CapaEntidad/Producto.cs
+++ CapaEntidad/Producto.cs
@@ -6,20 +6,21 @@ using System.Threading.Tasks;
 
 namespace CapaEntidad
 {
-        public class Producto
-        {
-            public int Id_Producto { get; set; }
-            public Categoria oCategoria { get; set; }
-            public string Nombre { get; set; }
-            public string Descripcion { get; set; }
-            public decimal Precio { get; set; }
-            public string PrecioTexto { get; set; }
-            public string Ruta_Imagen { get; set; }
-            public string Nombre_Imagen { get; set; }
-            public bool Activo { get; set; }
-            public string Base64 { get; set; }
-            public string Extension { get; set; }
+    public class Producto
+    {
+        public int Id_Producto { get; set; }
+        public Categoria oCategoria { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public string PrecioTexto { get; set; }
+        public string Ruta_Imagen { get; set; }
+        public string Nombre_Imagen { get; set; }
+        public bool Activo { get; set; }
+        public string Base64 { get; set; }
+        public string Extension { get; set; }
         public object Extra_Producto { get; set; }
-        public int Stock { get; set; }
+    
+        
     }
 }
